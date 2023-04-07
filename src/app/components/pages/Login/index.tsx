143e052cca6fc363d1c19e04533287b171d28ade
Keven Jesus <falecom@keven.com.br>
2023-04-07 11:27:19 -0300

    feat: page login

---
+++ src/app/components/pages/Login/index.tsx
@@ -0,0 +1,75 @@
+'use client'
+
+import TextField from '@/app/DesignSystem/TextField'
+import * as S from './style'
+import { ChangeEvent, useCallback, useState } from 'react'
+import Button from '@/app/DesignSystem/Button'
+import Alert from '@/app/DesignSystem/Alert'
+import { MessageTypes } from '@/app/types'
+import { fetchLogin } from '@/app/services/login'
+import { useRouter } from 'next/navigation'
+
+export default function LoginPage() {
+    const [fieldLogin, setLogin] = useState("")
+    const [fieldPassword, setPassword] = useState("")
+    const [message, setMessage] = useState<MessageTypes | null>(null)
+    const route = useRouter()
+
+    const handleLogin = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        setLogin(event.target.value)
+    }, [])
+
+    const handlePassword = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }, [])
+
+    const clearPassword = useCallback(() => {
+        setPassword("")
+    }, [])
+
+    const validFields = useCallback(() => {
+        if(!fieldLogin) {
+            setMessage({type: 'error' , message: 'Por favor. Informe o campo login'})
+            return false
+        }else if(!fieldPassword) {
+            setMessage({type: 'error' , message: 'Por favor. Informe o campo senha'})
+            return false
+        }else {
+            setMessage(null)
+            return true
+        }
+    }, [fieldLogin, fieldPassword, setMessage])
+
+    const submiting = useCallback((event: ChangeEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if(validFields()) {
+            const isLoggin = fetchLogin(fieldLogin, fieldPassword)
+            if(isLoggin) {
+                setMessage({type: 'success', message: 'Bem-vindo(a), voce será redirecionado(a)'})
+                sessionStorage.setItem("isLoggin", "true")
+                setTimeout(() => {
+                    route.push('/')
+                }, 1000)
+            }else {
+                setMessage({type: 'error', message: 'Usuário e/ou senha inválidos'})
+                clearPassword()
+            }
+        }
+    }, [validFields, route])
+
+    return (
+        <S.Wrapper>
+            <S.Logo width={160} src='/heroverse.png' alt="Hero Verse" />
+            <S.Form onSubmit={submiting}>
+                <TextField type="text" name="login" placeholder='Login' value={fieldLogin} onChange={handleLogin} />
+                <TextField type="password" name="password" placeholder='Senha' value={fieldPassword} onChange={handlePassword} />
+                <Button type="submit" variant='primary'>Entrar</Button>
+                {
+                    message && (
+                        <Alert type={message.type}>{message.message}</Alert>
+                    )
+                }
+            </S.Form>
+        </S.Wrapper>
+    )
+}
---
+++ src/app/components/pages/Login/style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    gap: 60px;
+    padding: 60px 16px;
+`
+export const Logo = styled.img`
+    display: block;
+    margin: 0 auto;
+`
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 23px;
+    button {
+        margin-top: 13px;
+    }
+`
