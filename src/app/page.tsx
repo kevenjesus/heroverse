00769390c1d23ec646c3a1d26b63cd7173212a7b
Keven Jesus <falecom@keven.com.br>
2023-04-10 09:50:37 -0300

    test complie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
 import HomePage from "./components/pages/Home";
-import { getAllCategories } from "./services/category";
-import { getAllHeroes } from "./services/hero";
-import { ReturnFetchType } from "./services/types";
+import { headers, returnResponse } from "./services/config";
+
+async function getAllCategories() {
+    const request = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/Category`, {
+        method: 'GET',
+        headers,
+        cache: 'no-store',
+        next: {
+            revalidate: 0
+        }
+    })
+    const response = await request.json()
+    return returnResponse(request, response, 200)
+}
+
+async function getAllHeroes() {
+    const request = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/Heroes`, {
+        method: 'GET',
+        headers,
+        cache: 'no-store',
+        next: {
+            revalidate: 0
+        }
+    })
+    const response = await request.json()
+    return returnResponse(request, response, 200)
+}
 
 export default async function Home() {
     const [categories, heroes] = await Promise.all([getAllCategories(), getAllHeroes()])
